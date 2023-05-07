b61d53e148e785dfab6804b6863721188d0a888b
AR123456 <coloradoz99@gmail.com>
2023-05-07 08:39:33 -0600

    post request set up

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 // endpoint
 // POST https://api.openai.com/v1/completions
 
-fetch("https://api.openai.com/v1/completions", {
-  method: "POST",
-  headers: {
-    Authorization: `Bearer ${API_Key}`,
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({
-    model: "text-davinci-003",
-    prompt: "hello,how are you today",
-    max_tokens: 7,
-  }),
-});
+// fetch("https://api.openai.com/v1/completions", {
+//   method: "POST",
+//   headers: {
+//     Authorization: `Bearer ${API_Key}`,
+//     "Content-Type": "application/json",
+//   },
+//   body: JSON.stringify({
+//     model: "text-davinci-003",
+//     prompt: "hello,how are you today",
+//     max_tokens: 7,
+//     temperature: 0,
+//   }),
+// });
+
+async function fetchData() {
+  const response = await fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: "hello,how are you today",
+      max_tokens: 7,
+      temperature: 0,
+    }),
+  });
+}
