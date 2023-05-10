6b3e9d56877d5cfb3f5c07ca38f4412707c71180
AR123456 <coloradoz99@gmail.com>
2023-05-10 06:45:49 -0600

    just oepn dev tools for now

--- app.js
+++ app.js
@@ -1,46 +1,22 @@
-const express = require("express");
-const fetch = require("node-fetch");
-const app = express();
+// const API_KEY = process.env.API_KEY;
+const API_KEY = "s";
 
-// app.post("/example", async (req, res) => {
-//   try {
-//     const response = await fetch("https://jsonplaceholder.typicode.com/posts", {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({
-//         title: "Example Post",
-//         body: "This is an example post.",
-//         userId: 1,
-//       }),
-//     });
-//     const json = await response.json();
-//     res.send(json);
-//   } catch (err) {
-//     console.log(err);
-//     res.status(500).send("Error");
-//   }
-// });
-
-// async function fetchData() {
-//   const response = await fetch("https://api.openai.com/v1/completions", {
-//     method: "POST",
-//     headers: {
-//       Authorization: `Bearer ${API_KEY}`,
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       model: "text-davinci-003",
-//       prompt: "hello,how are you today",
-//       max_tokens: 7,
-//       temperature: 0,
-//     }),
-//   });
-//   const data = await response.json();
-//   console.log(data);
-// }
-
-// fetchData();
-
-module.exports = app;
+async function fetchData() {
+  const response = await fetch("https://api.openai.com/v1/completions", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      model: "text-davinci-003",
+      prompt: "hello,how are you today",
+      max_tokens: 7,
+      temperature: 0,
+    }),
+  });
+  const data = await response.json();
+  console.log(data);
+}
+// will be called when web page opened page and look at console in inspector tools
+fetchData();
