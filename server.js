5765586caf2487082ce33889d110e09d4e514534
AR123456 <coloradoz99@gmail.com>
2023-05-10 06:45:11 -0600

    looking at server set up

---
+++ server.js
@@ -0,0 +1,15 @@
+// const express = require("express");
+const dotenv = require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+const app = require("./app.js");
+
+// const app = express();
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.get("/", (req, res) => {
+//   res.send("Hello World!");
+// });
+app.listen(PORT, () =>
+  console.log(`Server started on port http://localhost:${PORT}`)
+);
