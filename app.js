3f49a479000821e4163b890d4151c7862394492c
AR123456 <coloradoz99@gmail.com>
2023-05-11 06:36:07 -0600

    setting up try catch

--- app.js
+++ app.js
@@ -1,27 +1,31 @@
 // const API_KEY = process.env.API_KEY;
 const API_KEY = "";
 
-async function fetchData() {
-  const response = await fetch("https://api.openai.com/v1/chat/completions", {
-    method: "POST",
-    headers: {
-      Authorization: `Bearer ${API_KEY}`,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      // model: "gpt-4", // this did not work but the turbo did
-      model: "gpt-3.5-turbo",
+async function getMessage() {
+  try {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        // model: "gpt-4", // this did not work but the turbo did
+        model: "gpt-3.5-turbo",
 
-      messages: [
-        {
-          role: "user",
-          content: "Hello!",
-        },
-      ],
-    }),
-  });
-  const data = await response.json();
-  console.log(data);
+        messages: [
+          {
+            role: "user",
+            content: "Hello!",
+          },
+        ],
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    //
+  }
 }
 // will be called when web page opened page and look at console in inspector tools
-fetchData();
+getMessage();
