164543740244b1be189a55bfd56764e7ddd89a14
AR123456 <coloradoz99@gmail.com>
2023-05-12 02:45:26 -0600

    using options const

--- app.js
+++ app.js
@@ -4,33 +4,34 @@ const API_KEY = "";
 const submitButton = document.getElementById("submit");
 
 async function getMessage() {
-  try {
-    const options = {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${API_KEY}`,
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        // model: "gpt-4", // this did not work but the turbo did
-        model: "gpt-3.5-turbo",
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      // model: "gpt-4", // this did not work but the turbo did
+      model: "gpt-3.5-turbo",
 
-        messages: [
-          {
-            role: "user",
-            content: "Hello!",
-          },
-        ],
-      }),
-    };
+      messages: [
+        {
+          role: "user",
+          content: "Hello!",
+        },
+      ],
+      max_tokens: 100,
+    }),
+  };
+  try {
     const response = await fetch(
       "https://api.openai.com/v1/chat/completions",
-      {}
+      options
     );
     const data = await response.json();
     console.log(data);
   } catch (error) {
-    //
+    console.log(error);
   }
 }
 
