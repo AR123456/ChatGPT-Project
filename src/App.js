55350b12813ba6e0fe71394779980fd8dff927e1
AR123456 <coloradoz99@gmail.com>
2023-05-18 06:50:00 -0600

    react app set up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 const App = () => {
-  return <div className="app">My app</div>;
+  return (
+    <div className="app">
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>Made by Anne</p>
+        </nav>
+      </section>
+      <section className="main"></section>
+    </div>
+  );
 };
 
 export default App;
