6c2606b3c85fe742136d4116001e04c43cca8399
AR123456 <coloradoz99@gmail.com>
2023-05-19 06:59:27 -0600

    css nav element

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ const App = () => {
     <div className="app">
       <section className="side-bar">
         <button>+ New Chat</button>
-        <ul className="history"></ul>
+        <ul className="history">
+          <li>List Start</li>
+        </ul>
         <nav>
           <p>Made by Anne</p>
         </nav>
