7a8af42c5ceadf2d8f55f0edbb55e1313a39756d
AR123456 <coloradoz99@gmail.com>
2023-05-20 02:28:23 -0600

    set up route

--- server.js
+++ server.js
@@ -8,5 +8,14 @@ app.use(express.json());
 app.use(cors());
 
 // api key here
+const API_KEY = process.env.API_KEY;
+app.post("/completions", (req, res) => {
+  try {
+    // node fetch API
+    fetch("");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 app.listen(PORT, () => console.log("Server is running on port", PORT));
