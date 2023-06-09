98dbe920022c601a46765d186bb7e61be13e45b4
AR123456 <coloradoz99@gmail.com>
2023-06-09 06:48:36 -0600

    getting image back

--- app.js
+++ app.js
@@ -2,72 +2,20 @@
 const API_KEY = "";
 
 async function fetchImages() {
-  fetch("https://api.openai.com/v1/images/generations", {
+  const response = await fetch("https://api.openai.com/v1/images/generations", {
     method: "POST",
     headers: {
       Authorization: `Bearer ${API_KEY}`,
       "Content-Type": "application/json",
     },
+    body: JSON.stringify({
+      prompt: "A hamburger singing in the rain",
+      n: 2,
+      size: "1024x1024",
+    }),
   });
+  const data = await response.json();
+  console.log(data);
+  console.log(data.data[0]);
 }
-
-// const submitButton = document.getElementById("submit");
-// const outputElement = document.getElementById("output");
-// const inputElement = document.querySelector("input");
-// const historyElement = document.querySelector(".history");
-// const buttonElement = document.querySelector("button");
-
-// function changeInput(value) {
-//   const inputElement = document.querySelector("input");
-//   inputElement.value = value;
-// }
-
-// async function getMessage() {
-//   const options = {
-//     method: "POST",
-//     headers: {
-//       Authorization: `Bearer ${API_KEY}`,
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({
-//       // model: "gpt-4", // this did not work but the turbo did
-//       model: "gpt-3.5-turbo",
-//       messages: [
-//         {
-//           role: "user",
-//           content: inputElement.value,
-//         },
-//       ],
-//       // max number of words or characters that can be generated in response
-//       max_tokens: 500,
-//     }),
-//   };
-//   try {
-//     const response = await fetch(
-//       "https://api.openai.com/v1/chat/completions",
-//       options
-//     );
-//     const data = await response.json();
-//     console.log(data);
-//     outputElement.textContent = data.choices[0].message.content;
-//     if (data.choices[0].message.content) {
-//       const pElement = document.createElement("p");
-//       pElement.textContent = inputElement.value;
-//       //if the click is outside the text box bring the cursor there
-//       pElement.addEventListener("click", () =>
-//         changeInput(pElement.textContent)
-//       );
-//       historyElement.append(pElement);
-//     }
-//     // pass the entered question to history
-//   } catch (error) {
-//     console.log(error);
-//   }
-// }
-// submitButton.addEventListener("click", getMessage);
-// function clearInput() {
-//   // replace what is in div with empty string
-//   inputElement.value = "";
-// }
-
-// buttonElement.addEventListener("click", clearInput);
+fetchImages();
