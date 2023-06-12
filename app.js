aba05783b498c3e280af1665b938a2afa3638a7d
AR123456 <coloradoz99@gmail.com>
2023-06-12 06:09:46 -0600

    image generation options

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
 // const API_KEY = process.env.API_KEY;
-const API_KEY = "";
+const API_KEY = " ";
 const submitIcon = document.querySelector("#submit-icon");
 
-const getImages = () => {
+const getImages = async () => {
   try {
-    async function fetchImages() {
-      const response = await fetch(
-        "https://api.openai.com/v1/images/generations",
-        {
-          method: "POST",
-          headers: {
-            Authorization: `Bearer ${API_KEY}`,
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            prompt: "A hamburger singing in the rain",
-            n: 2,
-            size: "1024x1024",
-          }),
-        }
-      );
-      const data = await response.json();
-      console.log(data);
-      console.log(data.data[0]);
-    }
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${API_KEY}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          prompt: "A hamburger singing in the rain",
+          n: 2,
+          size: "1024x1024",
+        }),
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+    console.log(data.data[0]);
   } catch (error) {
     console.log(error);
   }
