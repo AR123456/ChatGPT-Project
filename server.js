bda4ec6a102021c56339418a0a2044da2e1132be
AR123456 <coloradoz99@gmail.com>
2023-06-13 06:49:48 -0600

    server work

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// const fetch = require("node-fetch");
+const fetch = require("node-fetch");
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
@@ -12,5 +12,31 @@ app.use(express.json());
 app.use(cors());
 
 const API_KEY = process.env.API_KEY;
+app.post("/generations", async (req, res) => {
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      // input from front end
+      prompt: req.body.inputValue,
+      n: 4,
+      size: "1024x1024",
+    }),
+  };
+  try {
+    // node fetch API
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      options
+    );
+    const data = await response.json();
+    res.send(data);
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 app.listen(PORT, () => console.log("Server is running on port", PORT));
