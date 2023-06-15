78e5a6074d34f920e031896d3f21ea3db6b41526
AR123456 <coloradoz99@gmail.com>
2023-06-15 05:59:05 -0600

    all kinds of crashes

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// const fetch = require("node-fetch");
+const fetch = require("node-fetch");
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
@@ -40,5 +40,38 @@ const API_KEY = process.env.API_KEY;
 //     console.error(error);
 //   }
 // });
+const getImages = async () => {
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${API_KEY}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: "pink cat",
+      n: 4,
+      size: "1024x1024",
+    }),
+  };
+  try {
+    const response = await fetch(
+      "https://api.openai.com/v1/images/generations",
+      options
+    );
+    const data = await response.json();
+    console.log(data);
+    console.log(data.data[0]);
+    // data?.data.forEach((imageObject) => {
+    //   const imageContainer = document.createElement("div");
+    //   imageContainer.classList.add("image-container");
+    //   const imageElement = document.createElement("img");
+    //   imageElement.setAttribute("src", imageObject.url);
+    //   imageContainer.append(imageElement);
+    //   imageSection.append(imageContainer);
+    // });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 app.listen(PORT, () => console.log("Server is running on port", PORT));
