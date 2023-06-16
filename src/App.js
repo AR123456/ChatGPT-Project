ce9514857a8371cf0d165eea79bd07a4abc95183
AR123456 <coloradoz99@gmail.com>
2023-06-16 06:46:08 -0600

    bare bones app

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ const App = () => {
         <p>
           Description <span className="suprise">Suprise me</span>
         </p>
+        <div className="input-container">
+          <input placeholder="An impressionist oil painting of a sunflower in a purple vase" />
+          <button>Generate</button>
+        </div>
       </section>
       <section className="image-section"></section>
     </div>
