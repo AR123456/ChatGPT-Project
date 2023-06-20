6a09910b33a494aa6ce640c8b6c837fc38ed09f9
AR123456 <coloradoz99@gmail.com>
2023-06-20 06:20:20 -0600

    server and env

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const PORT = 8000;
 const express = require("express");
 const cors = require("cors");
-
 const app = express();
 
 app.use(cors());
+// sending json between front and back end
+app.use(express.json());
+// safely store vars
+require("dotenv").config();
+app.listen(PORT, () => console.log(`Your server is running on port: ${PORT}`));
