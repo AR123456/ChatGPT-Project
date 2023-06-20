5c7f9c9c8334e9dbd9b4de978d27811b097f6a18
AR123456 <coloradoz99@gmail.com>
2023-06-20 06:20:57 -0600

    server boilerplate from open api

--- server.js
+++ server.js
@@ -8,4 +8,17 @@ app.use(cors());
 app.use(express.json());
 // safely store vars
 require("dotenv").config();
+
 app.listen(PORT, () => console.log(`Your server is running on port: ${PORT}`));
+
+// getting from open AI  https://platform.openai.com/docs/api-reference/images/create?lang=node.js
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.createImage({
+  prompt: "A cute baby sea otter",
+  n: 2,
+  size: "1024x1024",
+});
