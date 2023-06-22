4f40cdcf84051682042dbf1125b5d8242e4dd00a
AR123456 <coloradoz99@gmail.com>
2023-06-22 06:40:39 -0600

    get image front end

--- src/App.js
+++ src/App.js
@@ -6,7 +6,24 @@ const App = () => {
     "A seal talking on a telephone",
     "A pineapple swiming in a pond",
   ];
-
+  const getImages = async () => {
+    try {
+      const options = {
+        method: "POST",
+        body: JSON.stringify({
+          message: "CAT",
+        }),
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      const response = await fetch("http://localhost:8000/images", options);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
   return (
     <div className="app">
       <section className="search-section">
