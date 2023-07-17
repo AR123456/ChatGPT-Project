d228be774a7a8ec36420f0cf7ef85e0bf7b3a926
AR123456 <coloradoz99@gmail.com>
2023-07-17 06:16:04 -0600

    multer on backend

--- server.js
+++ server.js
@@ -10,11 +10,24 @@ app.use(express.json());
 require("dotenv").config();
 // getting from open AI  https://platform.openai.com/docs/api-reference/images/create?lang=node.js
 const { Configuration, OpenAIApi } = require("openai");
+// packages required for file upload
+const fs = require("fs");
+const multer = require("multer");
 const configuration = new Configuration({
   apiKey: process.env.API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
+// where to store files
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    // were to save to
+    cb(null, "public");
+  },
+  // how should the file name look ?
+});
+
+const upload = multer({ storage: storage }).single("file");
 
 app.post("/images", async (req, res) => {
   try {
