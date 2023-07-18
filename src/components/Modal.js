dd3a8f6d2984f221754de7f28a87e8bb374a6ffc
AR123456 <coloradoz99@gmail.com>
2023-07-18 07:03:06 -0600

    setting up preview modal

---
+++ src/components/Modal.js
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const Modal = () => {
+  const [error, setError] = useState(null);
+  return (
+    <div className="modal">
+      <div>modal</div>
+    </div>
+  );
+};
+
+export default Modal;
