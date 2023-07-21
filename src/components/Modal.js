3f27f389e0a785e68a0c5950dc9a87ac13ba070b
AR123456 <coloradoz99@gmail.com>
2023-07-21 07:16:39 -0600

    size error checking

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,6 +2,7 @@ import { useState, useRef } from "react";
 
 const Modal = ({ setModalOpen, setSelectedImage, selectedImage }) => {
   const [error, setError] = useState(null);
+  const ref = useRef(null);
 
   const closeModal = () => {
     setModalOpen(false);
@@ -9,6 +10,11 @@ const Modal = ({ setModalOpen, setSelectedImage, selectedImage }) => {
   };
   const checkSize = () => {
     console.log("selectedImage", selectedImage);
+    if (ref.current.width === 256 && ref.current.height === 256) {
+      // generateVariations()
+    } else {
+      setError("Error: Choose 256 X 256 image");
+    }
   };
   return (
     <div className="modal">
@@ -16,11 +22,16 @@ const Modal = ({ setModalOpen, setSelectedImage, selectedImage }) => {
       <div className="img-container">
         {selectedImage && (
           // eslint-disable-next-line jsx-a11y/img-redundant-alt
-          <img src={URL.createObjectURL(selectedImage)} alt="uploaded image" />
+          <img
+            ref={ref}
+            src={URL.createObjectURL(selectedImage)}
+            alt="uploaded image"
+          />
         )}
       </div>
-      <button onClick={checkSize}>Click to check size </button>
-      <button>Generate</button>
+      <p>{error || "*Image must be 256 x 256"}</p>
+      {!error && <button onClick={checkSize}>Generate </button>}
+      {error && <button onClick={closeModal}> Close and try again</button>}
     </div>
   );
 };
