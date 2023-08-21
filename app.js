bbab5fed1a26be7cc7ed898561bf0b4b8b1b12ad
AR123456 <coloradoz99@gmail.com>
2023-08-21 05:40:21 -0600

    clean up

--- app.js
+++ app.js
@@ -5,15 +5,6 @@ import colors from "colors";
 
 // create completion is asyncornis so need async await syntax
 async function main() {
-  // const chatCompletion = await openai.createChatCompletion({
-  //   model: "gpt-3.5-turbo",
-  //   messages: [{ role: "user", content: "What is the capital of Florida" }],
-  // });
-  // console.log(chatCompletion.data.choices[0].message.content);
-  // readline sync stuff
-  // const userName = readlineSync.question("May I have your name?");
-  // console.log(`Hello ${userName}`);
-  // colors
   console.log(colors.bold.green("Welcome to the Chatbot Program"));
   console.log(colors.bold.green("You can start chatting with the bot "));
   // want to keep the coversation going
