45840dad3ce5af063577fe0126e1e75b00e6ed95
AR123456 <coloradoz99@gmail.com>
2023-08-24 03:43:56 -0600

    err

--- app.js
+++ app.js
@@ -7,15 +7,24 @@ import colors from "colors";
 async function main() {
   console.log(colors.bold.green("Welcome to the Chatbot Program"));
   console.log(colors.bold.green("You can start chatting with the bot "));
+  // chat history so the bot can track with a conversation
+  const chatHistory = [];
+
   // want to keep the coversation going
   while (true) {
     // get input from user
     const userInput = readlineSync.question(colors.yellow("You: "));
+    // iterate over history to construct messages
+    const messages = chatHistory.map(([role, content]) => [role, content]);
+    // add latest user input to the array
+    messages.push({ role: "user", content: userInput });
+
     try {
       // call api with user input
       const chatCompletion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [{ role: "user", content: userInput }],
+        // now pass the messages array
+        messages: messages,
       });
       const completionText = chatCompletion.data.choices[0].message.content;
       // add way to escape the while loop
@@ -25,6 +34,9 @@ async function main() {
         return;
       }
       console.log(colors.green("Bot: ") + completionText);
+      // also need to store the bots responses
+      chatHistory.push(["user", userInput]);
+      chatHistory.push(["assistant", completionText]);
     } catch (error) {
       console.error(colors.red(error));
     }
